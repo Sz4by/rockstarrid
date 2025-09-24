574d6109910ebff2432de9f72b9bb3cbcc1fbe64
Szaby <szaby.korodi@yahoo.com>
2025-09-24 18:48:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// /n/ útvonal kezelése
+app.get('/n/:name', (req, res) => {
+  const name = req.params.name;
+  
+  // Itt bármilyen logikát beilleszthetsz, ami visszaadja a kívánt információkat.
+  // Például, ha egy adatbázis vagy API van a háttérben:
+  res.send(`<h1>Looking up name: ${name}</h1>`);
+});
+
 // Port beállítása
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
