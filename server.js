a789eb424fe840c8d858f1e20671dc92f862710d
Szaby <szaby.korodi@yahoo.com>
2025-09-24 19:06:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,28 +5,18 @@ const path = require('path');
 // Statikus fájlok (CSS, JS) kiszolgálása
 app.use(express.static(path.join(__dirname, '')));
 
-// Főoldal betöltése
+// Főoldal betöltése (index.html)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// /n/ útvonal kezelése, a query paraméterek kiolvasása
-app.get('/n', (req, res) => {
-  const name = req.query.name; // Kiolvassuk a name paramétert
-
-  if (name) {
-    res.send(`<h1>Looking up name: ${name}</h1>`);
-  } else {
-    res.send(`<h1>No name provided</h1>`);
-  }
-});
-
-// /r/ útvonal kezelése (példa a RID lekérdezéshez)
-app.get('/r/:rid', (req, res) => {
-  const rid = req.params.rid;
-  
-  res.send(`<h1>Looking up RID: ${rid}</h1>`);
-});
+// /n/ útvonal (nem szükséges, mert az API kérés a frontendről jön)
+// Ha szükséges, itt kezelhetjük a kereséseket
+// app.get('/n', (req, res) => {
+//   const name = req.query.name;
+//   // Itt elvégezhetjük a keresést, de mivel az API a frontendről jön, nem szükséges
+//   res.send(`<h1>Looking up: ${name}</h1>`);
+// });
 
 // Port beállítása
 const PORT = process.env.PORT || 3000;
